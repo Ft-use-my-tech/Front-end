f284a3058c979e6a13d367d012d313576c1b5ddb
dominthebox <dsallustro@gmail.com>
2021-06-24 14:40:18 -0400

    added active page color in nav bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,18 +6,37 @@ import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Profile from './components/Profile';
 // import signUpSchema from './schemas/CreateSchema';
-
+import { useLocation } from 'react-router-dom';
 
 
 function App() {
+
+  //assigning location variable
+  const location = useLocation();
+
+  //destructuring pathname from location
+  const { pathname } = location;
+
+  //Javascript split method to get the name of the path in array
+  const splitLocation = pathname.split("/");
+
+
   return (
     <div className='header'>
       <nav>
         <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/Login'>Login</Link></li>
-          <li><Link to='/SignUp'>Sign Up</Link></li>
-          <li><Link to='/Profile'>Profile</Link></li>
+          <li className={splitLocation[1] === "" ? "active" : ""}>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className={splitLocation[1] === "Login" ? "active" : ""}>
+            <Link to='/Login'>Login</Link>
+          </li>
+          <li className={splitLocation[1] === "SignUp" ? "active" : ""}>
+            <Link to='/SignUp'>Sign Up</Link>
+          </li>
+          <li className={splitLocation[1] === "Profile" ? "active" : ""}>
+            <Link to='/Profile'>Profile</Link>
+          </li>
         </ul>
       </nav>
       <h1>Use My Tech</h1>
